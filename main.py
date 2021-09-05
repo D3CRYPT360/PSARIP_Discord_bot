ccb78efbea145fa1c4be4e862104744c7a6245a5
D3crypt360 <nahilali3600@gmail.com>
2021-09-05 16:19:44 +0500

    Multiple channel support, PEP8 standard formatted.

--- main.py
+++ main.py
@@ -8,32 +8,53 @@ import glob
 from bs4 import BeautifulSoup
 
 
-CHANNEL_ID = # Insert channel ID
+CHANNEL_ID = []
 
 load_dotenv(".env")
 
 Last = None
 Current = None
 
-activity = discord.Activity(type=discord.ActivityType.watching, name="https://x265.club/")
-bot = commands.Bot(command_prefix="", activity=activity)
+activity = discord.Activity(
+    type=discord.ActivityType.watching,
+    name="https://x265.club/")
+bot = commands.Bot(command_prefix="=", activity=activity)
+
 
 @bot.event
 async def on_ready():
     print(f'Logged in as: {bot.user.name}')
     print(f'With ID: {bot.user.id}')
 
+
+async def channel_sender(TITLE, LINK, NAME, CHANNEL_ID):
+    channel_to_send = bot.get_channel(CHANNEL_ID)
+    embed = discord.Embed(
+        colour=discord.Color.random(),
+        title=TITLE,
+        # Getting the text in the P tag in the description
+        description=f"[LINK]({LINK})\n{soup.p.text}"
+    )
+
+    # For some weird reason discord won't put image directly on the embed from the link provided.
+    # Have to download it locally and upload it.
+    file = discord.File(f"./images/{NAME}", filename="image.png")
+    embed.set_image(url="attachment://image.png")
+    await channel_to_send.send(file=file, embed=embed)
+
+
 @tasks.loop(seconds=60)
 async def top():
-    channel0 = bot.get_channel(CHANNEL_ID)
     URL = "https://x265.club/feed"
     feed = feedparser.parse(URL)
 
-    DESC = feed.entries[0].summary_detail.value     
+    DESC = feed.entries[0].summary_detail.value
     TITLE = feed.entries[0].title
-    LINK = feed.entries[0].link  
+    LINK = feed.entries[0].link
 
-    # Parsing HTML to get the image link because the description is in HTML format
+    # Parsing HTML to get the image link because the description is in HTML
+    # format
+    global soup
     soup = BeautifulSoup(DESC, 'lxml')
     image = soup.find('img')
     IMAGE = image.get("src")
@@ -46,22 +67,16 @@ async def top():
         Last = Current
         await Img_Downloader(IMAGE, NAME)
 
-
-        embed = discord.Embed(
-            colour = discord.Color.random(),
-            title = TITLE,
-            description=f"[LINK]({LINK})\n{soup.p.text}" # Getting the text in the P tag in the description
-        )
-
-        # For some weird reason discord won't put image directly on the embed from the link provided.
-        # Have to download it locally and upload it. 
-        file = discord.File(f"./images/{NAME}", filename="image.png")
-        embed.set_image(url="attachment://image.png")
-        await channel0.send(file=file, embed=embed)
+        if len(CHANNEL_ID) > 1:
+            for channel in CHANNEL_ID:
+                await channel_sender(TITLE, LINK, NAME, channel)
+        else:
+            await channel_sender(TITLE, LINK, NAME, CHANNEL_ID[0])
 
         files = glob.glob("./images/*")
         for f in files:
             os.remove(f)
+
 
 @top.before_loop
 async def wait_for_bot():
